e7f38f6b2cae383f28d75b1afcb4b397111505ab
Javier5464 <131973021+Javier5464@users.noreply.github.com>
2023-10-09 22:09:54 +0200

    dados.php

--- dados.php
+++ dados.php
@@ -3,48 +3,123 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Piedra, Papel, Tijera</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>PIEDRA, PAPEL, TIJERA!</h1>
+    
+<?php
 
+    $dado =[
+        "dado1" => "&#9856",
+        "dado2" => "&#9857",
+        "dado3" => "&#9858",
+        "dado4" => "&#9859",
+        "dado5" => "&#9860",
+        "dado6" => "&#9861"
+    ];
+
+    $a =[
+        "dado1" => 1,
+        "dado2" => 2,
+        "dado3" => 3,
+        "dado4" => 4,
+        "dado5" => 5,
+        "dado6" => 6
+    ];
+
+    $b=[
+        "&#9856" => 1,
+        "&#9857" => 2,
+        "&#9858" => 3,
+        "&#9859" => 4,
+        "&#9860" => 5,
+        "&#9861" => 6
+    ];
+
+    $j1=[];
+
+    $j2=[];
+
+    $max1=0;
+    $max2=0;
+    $min1=7;
+    $min2=7; 
+
+    $r1=0;
+    $r2=0;
+
+    for($i=0;$i<5;$i++){
+        $random=array_rand($dado);
+        $x=$dado[$random];
+        array_push($j1,$x);
+
+        if($a[$random]>$max1){
+            $max1=$a[$random];
+        }
+
+        if($a[$random]<$min1){
+            $min1=$a[$random];
+        }
+
+        $r1=$r1 + $a[$random];
+    }
+
+    $r1=$r1-$max1 - $min1;
+
+
+
+    for($i=0;$i<5;$i++){
+        $random=array_rand($dado);
+        $x=$dado[$random];
+        array_push($j2,$x);
+
+        if($a[$random]>$max1){
+            $max2=$a[$random];
+        }
+
+        if($a[$random]<$min1){
+            $min2=$a[$random];
+        }
+
+        $r2=$r2 + $a[$random];
+    }
+
+    $r2=$r2-$max2 - $min2;
+
+    $msg = [
+        "Empate",
+        "Ha ganado el jugador 1",
+        "Ha ganado el jugador 2"
+    ];
+
+    function ganador($r1,$r2,$msg){
+        if($r1>$r2){
+            return $msg[1];
+        }
+        if($r1<$r2){
+            return $msg[2];
+        }else{
+            return $msg[0];
+        }
+    }
+
+?>
+
+    <h1>Cinco dados</h1>
+    <h3>Jugador 1 </h3> <h4><?php echo $r1 ?> puntos</h4>
     <?php
-        $tabla = [
-            [0, 2, 1],
-            [1, 0, 2],
-            [2, 1, 0]
-        ];
-
-        $msg = [
-            "Empate",
-            "Ha ganado el jugador 1",
-            "Ha ganado el jugador 2"
-        ];
-
-        $opciones = [
-            "piedra" => "&#x1F91C;",
-            "papel" => "&#x1F91A;",
-            "tijera" => "&#x1F596;"
-        ];
-
-        $a = [
-            "piedra" => 0,
-            "papel" => 1,
-            "tijera" => 2,
-        ];
-
-        $jugador1 = array_rand($opciones);
-        $jugador2 = array_rand($opciones);
-
-        function ganador($jugador1, $jugador2, $tabla, $msg, $a) {
-            $resultado = $tabla[$a[$jugador1]][$a[$jugador2]];
-            return $msg[$resultado];
+        for($i=0;$i<5;$i++){
+            echo '<span style="font-size: 70px; background-color: red"">' . $j2[$i] . '</span>';
         }
     ?>
-    <h3>Jugador 1: </h3><?php echo '<span style="font-size: 70px;">' . $opciones[$jugador1] . '</span>'; ?> 
-    <h3>Jugador 2: </h3><?php echo '<span style="font-size: 70px;">' . $opciones[$jugador2] . '</span>'; ?> <br><br><br>
-    <h3><?php echo ganador($jugador1, $jugador2, $tabla, $msg, $a)?></h3> 
-
+    <h3>Jugador 2 </h3> <h4><?php echo $r2 ?> puntos</h4>
+    <?php
+        for($i=0;$i<5;$i++){
+            echo '<span style="font-size: 70px; background-color: blue">' . $j2[$i] . '</span>';
+        }
+    ?>
+    <br><br>
+    <h3><?php echo ganador($r1,$r2,$msg)?></h3>
+    
 </body>
 </html>
-
